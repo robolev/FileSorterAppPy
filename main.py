3c68c51672815e1e1577beb96fa58887493867bf
robolev <92596597+robolev@users.noreply.github.com>
2023-08-17 14:10:05 +0200

    Aded creting folder if file dont match any directory

--- main.py
+++ main.py
@@ -20,24 +20,38 @@ class MainWindow(QtWidgets.QMainWindow):
         if root_directory:
             self.ui.lineEdit.setText("Sorting files...")
 
-            entries = os.listdir(root_directory)
-
-            all_files = [os.path.join(root, file) for root, dirs, files in os.walk(root_directory) for file in files]
-            all_dirs = [os.path.join(root, dir) for root, dirs, files in os.walk(root_directory) for dir in dirs]
+            all_files, all_dirs = self.collect_files_and_dirs(root_directory)
 
             print("All Files:", all_files)
             print("All Directories:", all_dirs)
 
-            for file_path in all_files:
-                file_name, _ = os.path.splitext(os.path.basename(file_path))
-                for dir_path in all_dirs:
-                    dir_name = os.path.basename(dir_path)
-                    if file_name == dir_name:
-                        new_file_path = os.path.join(dir_path, os.path.basename(file_path))
-                        os.rename(file_path, new_file_path)
-                        break
+            self.move_files_to_matching_dirs(root_directory, all_files, all_dirs)
 
             self.ui.lineEdit.setText("Sorting completed.")
+
+    def collect_files_and_dirs(self, root_directory):
+        all_files = [os.path.join(root, file) for root, dirs, files in os.walk(root_directory) for file in files]
+        all_dirs = [os.path.join(root, dir) for root, dirs, files in os.walk(root_directory) for dir in dirs]
+        return all_files, all_dirs
+
+    def move_files_to_matching_dirs(self, root_directory, all_files, all_dirs):
+        for file_path in all_files:
+            file_name, _ = os.path.splitext(os.path.basename(file_path))
+            found_matching_dir = False
+
+            for dir_path in all_dirs:
+                dir_name = os.path.basename(dir_path)
+                if file_name == dir_name:
+                    new_file_path = os.path.join(dir_path, os.path.basename(file_path))
+                    os.rename(file_path, new_file_path)
+                    found_matching_dir = True
+                    break
+
+            if not found_matching_dir:
+                new_dir_path = os.path.join(root_directory, file_name)
+                os.makedirs(new_dir_path, exist_ok=True)
+                new_file_path = os.path.join(new_dir_path, os.path.basename(file_path))
+                os.rename(file_path, new_file_path)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
